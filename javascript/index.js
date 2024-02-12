5e2a6522c59c30db724825139df33b64d75d16b5
Elijah Darkeh <elijahagbedam18@gmail.com>
2024-02-12 08:53:57 -0800

    scroll to video modified with explanation

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,13 @@
-function scrollToVideo() {
-    const videoDiv = document.getElementsByClassName("video")[0];
+function scrollAndPlayVideo() {
+    //get element
+    const videoElement = document.querySelector('.video iframe');
+    if (videoElement) {
+        //get the parent element of the video
+        const videoContainer = videoElement.parentElement;
 
-    if (videoDiv) {
-        videoDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
-
-       
-        const videoIframe = videoDiv.querySelector('iframe');
-
-      
-        if (videoIframe && videoIframe.contentWindow) {
-            videoIframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-        }
+        //Offset is tlike the position (The OR sign means assign (0,0))
+        const scrollOffset = videoContainer?.offsetTop || 0;
+        //Scroll to is an event or method from the window  object(JS is powerful that it communicates with the window u are)
+        window.scrollTo({ top: scrollOffset, behavior: 'smooth' });
     }
 }
-
