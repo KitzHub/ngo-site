552454be5aaca4f77d5de8a3333aae3fc9bb2bbc
Elijah Darkeh <elijahagbedam18@gmail.com>
2024-03-04 07:33:00 -0800

    added  drop down

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,23 @@ function scrollAndPlayVideo() {
 
         //Offset is tlike the position (The OR sign means assign (0,0))
         const scrollOffset = videoContainer?.offsetTop || 0;
-        //Scroll to is an event or method from the window  object(JS is powerful that it communicates with the window u are)
+        //Scroll to is an event or method from the window  object(JS is powerful that it communicates with the window)
         window.scrollTo({ top: scrollOffset, behavior: 'smooth' });
     }
 }
+
+function displayActivityDropDown() {
+    const activityDropdown = document.querySelector(".activities-drop");
+    if (activityDropdown.style.display === "block") {
+        activityDropdown.style.display = "none";
+    } else {
+        activityDropdown.style.display = "block";
+    }
+}
+
+// function takeActivityDropdownOff() {
+//     const activityDropdown = document.querySelector(".activities-drop");
+//     if (activityDropdown.style.display === "block") {
+//         activityDropdown.style.display = "none";
+//     }
+// }
